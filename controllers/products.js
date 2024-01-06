58bc0c775c609aac422144e068900dfcee03f381
= <mauryasourav998@gmail.com>
2024-01-06 12:52:06 +0530

    cors

--- controllers/products.js
+++ controllers/products.js
@@ -116,7 +116,7 @@ const getImage = (req, res) => {
   console.log(filePath)
   if (fs.existsSync(filePath)) {
     // If the file exists, send it back in the response
-    res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
+    // res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
     res.sendFile(filePath)
   } else {
     res.status(404).send('File not found.')
