af9ce3b496315ecbf3ddae55a454f64c8b859ea1
= <mauryasourav998@gmail.com>
2024-01-06 13:38:05 +0530

    cors

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.set('trust proxy', 1)
 // )
 
 const corsOptions = {
-  origin: 'http://localhost:8888',
+  origin: '*',
   // origin: 'http://localhost:5173',
   // origin: 'https://comfyslothupgrad.netlify.app',
   optionsSuccessStatus: 200,
